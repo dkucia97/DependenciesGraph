09c4f89b421eb91d6a70c30ff5b9d5c2d249a702
DESKTOP-S1IQFSU\mkolisz11 <mkolisz11@gmail.com>
2019-10-14 18:28:56 +0200

    Fixed file opening and parsing

--- graph.py
+++ graph.py
@@ -1,16 +1,16 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import os
 from os import listdir
 import sample
 
 
-
-def createGraph(path="./"):
+def createGraph(path=os.path.join( "C:\\Users\\mkolisz11\\Desktop\\test" )):
     g=nx.DiGraph() # create direct graph 
     files_to_parse=list(filter(lambda f: f.endswith(".py") ,listdir(path))) #only python files
     for file in files_to_parse:
-        g.add_node(extract_filename(file))  
-        find_edges_in_file(file,g)
+        g.add_node(extract_filename(file))
+        find_edges_in_file(file,g,path)
     return g
     
     
@@ -21,12 +21,17 @@ def drawGraph(graph):
     plt.show()
 
 
-def find_edges_in_file(file,g):
-    with  open(file, 'r') as fr:
+def find_edges_in_file(file,g,path):
+    with  open(path+"\\"+file, 'r') as fr:
         for number, line in enumerate(fr):
-            if("import" in line ):
+            if("from" in line):
                 tab=line.split()
-                g.add_edge(extract_filename(file),tab[1])
+                if(tab[0]=="from"):
+                    g.add_edge(extract_filename(file),tab[3])
+            elif("import" in line):
+                tab=line.split()
+                if(tab[0]=="import"):
+                    g.add_edge(extract_filename(file),tab[1])
 
             
 def extract_filename(file):
--- sample.py
+++ sample.py
@@ -1,1 +1,2 @@
 import graph as g
+import array
