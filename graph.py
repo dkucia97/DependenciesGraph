472dcb0fd911b8759da5c7304be1937e7f8a6f11
ubijaczziemniaka <56658023+ubijaczziemniaka@users.noreply.github.com>
2019-10-19 17:58:58 +0200

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,5 +1,5 @@
-import networkx as nx
-import matplotlib.pyplot as plt
+import networkx 
+import matplotlib.pyplot 
 import os
 from os import listdir
 import sample
@@ -13,36 +13,46 @@ def loadFolder():
     return path
 
 def createGraph(path=loadFolder()):
-    g=nx.DiGraph() # create direct graph 
+    g=networkx.DiGraph() # create direct graph 
     files_to_parse=list(filter(lambda f: f.endswith(".py") ,listdir(path))) #only python files
+    
     for file in files_to_parse:
-        g.add_node(extract_filename(file))
+        g.add_node(extract_filename(file),waga = extract_filesize(file,g,path))
         find_edges_in_file(file,g,path)
-    return g
-   
+
+    matplotlib.pyplot.figure()
+    pos = networkx.spring_layout(g)
+    networkx.draw(g,pos, with_labels=True, font_weight='bold')
+    
+    pos_attr = {}
+    for node, coords in pos.items():
+        pos_attr[node] = (coords[0], coords[1] +00.07)
+    
+    node_attr = networkx.get_node_attributes(g, 'waga')
+    custom_node_attrs = {}
+    for node, attr in node_attr.items():
+        custom_node_attrs[node] = str(attr)
+
+    networkx.draw_networkx_labels(g,pos_attr, labels=custom_node_attrs)
+
+    edge_labels = dict([((u,v),d['weight']) for u,v,d in g.edges(data=True)])
+    networkx.draw_networkx_edge_labels(g,pos,edge_labels = edge_labels)
+    
+    matplotlib.pyplot.show()
 
 
-def drawGraph(graph):
-    pos = nx.spring_layout(graph)
-    nx.draw(graph,pos, with_labels=True, font_weight='bold')
-    edge_labels = dict([((u,v),d['weight']) for u,v,d in graph.edges(data=True)])
-    nx.draw_networkx_edge_labels(graph,pos,edge_labels = edge_labels)
-    plt.show()
-
-
-def find_edges_in_file(file,g,folderPath):
-    filePath = folderPath+"/"+file
-    with  open(filePath, 'r') as fr:
+def find_edges_in_file(file,g,path):
+    with  open(path+"/"+file, 'r') as fr:
         for number, line in enumerate(fr):
             if("from" in line):
                 tab=line.split()
                 if(tab[0]=="from"):
-                    w = sample.count_import(filePath,tab[3])
-                    g.add_edge(extract_filename(file),tab[3], weight = w)
+                    w = sample.count_import(file,tab[1])
+                    g.add_edge(extract_filename(file),tab[1], weight = w)
             elif("import" in line):
                 tab=line.split()
                 if(tab[0]=="import"):
-                    w = sample.count_import(filePath,tab[1])
+                    w = sample.count_import(file,tab[1])
                     print(w)
                     g.add_edge(extract_filename(file),tab[1],weight = w)
 
@@ -50,5 +60,9 @@ def find_edges_in_file(file,g,folderPath):
 def extract_filename(file):
     return file.split(".")[0]  #cut extension .py
 
-g=createGraph()
-drawGraph(g)
+
+def extract_filesize(file,g,folderPath):
+    return os.path.getsize(folderPath+"/"+file)
+
+
+createGraph()
