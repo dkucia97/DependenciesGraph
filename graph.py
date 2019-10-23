4ced10f1dfd5e6518ab316d2f81898c62ef07daa
DESKTOP-S1IQFSU\mkolisz11 <mkolisz11@gmail.com>
2019-10-23 22:20:25 +0200

    creating graph edges in create_functions_graph

--- graph.py
+++ graph.py
@@ -42,51 +42,6 @@ def createGraph(path="./"):
     
     matplotlib.pyplot.show()
 
-
-def get_python_files(path):
-    return list(filter(lambda f: f.endswith(".py") ,listdir(path)))
-
-def create_function_graph(path="./"):
-    g=networkx.DiGraph() # create direct graph 
-    files_to_parse=get_python_files(path) #only python files
-    for file in files_to_parse:
-        functions=get_functions_names_from_file(path+"/"+file)
-        for fun in functions :
-            g.add_node(fun)
-            find_function_edges(g,fun) # Todo
-
-
-    networkx.draw(g, with_labels=True, font_weight='bold')
-    matplotlib.pyplot.show()
-
-
-def get_functions_names_from_file(path):
-    res=[]
-    with open(path, 'r') as fr:
-         for number, line in enumerate(fr):
-             if re.match(r"^\s*?def",line) :
-                 res.append(line.split(" ")[1].split("(")[0])
-    return res
-
-def find_function_edges(graph,node): #todo
-    pass
-
-def count_method(file, method, name):
-    tmp = 0
-    t = 0
-    str = 'def ' + method
-    f = open(file,"r")
-    for x in f:
-        if t == 1:
-            if name in x:
-                tmp = tmp + 1
-            if 'def ' in x:
-                t = 0
-        elif str in x:
-            t = 1    
-    f.close()
-    return tmp
-
 def find_edges_in_file(file,g,path):
     with  open(path+"/"+file, 'r') as fr:
         for number, line in enumerate(fr):
@@ -111,5 +66,62 @@ def extract_filesize(file,g,folderPath):
     return os.path.getsize(folderPath+"/"+file)
 
 
+def get_python_files(path):
+    return list(filter(lambda f: f.endswith(".py") ,listdir(path)))
+
+def create_function_graph(path="./"):
+    g=networkx.DiGraph() # create direct graph 
+    files_to_parse=get_python_files(path) #only python files
+    functions=[]
+    #creating nodes
+    for file in files_to_parse:
+        functions+=get_functions_names_from_file(path+"/"+file)
+        for fun in functions :
+            g.add_node(fun)
+    #creating edges
+    for file in files_to_parse:
+        for fun in functions :
+            #print("   "+fun)
+            for otherFun in functions:
+                methodCount=count_method(path+"/"+file,fun,otherFun)
+                if(methodCount>0):
+                    #print("      "+otherFun)
+                    #print("         "+str(methodCount))
+                    g.add_edge(fun,otherFun,weight = 5)
+
+
+
+    networkx.draw(g, with_labels=True, font_weight='bold')
+    matplotlib.pyplot.show()
+
+
+def get_functions_names_from_file(path):
+    res=[]
+    with open(path, 'r') as fr:
+         for number, line in enumerate(fr):
+             if re.match(r"^\s*?def",line) :
+                 res.append(line.split(" ")[1].split("(")[0])
+    return res
+
+def count_method(path, method, otherMethod):
+    count = 0
+    t = 0
+    str = 'def ' + method
+    f = open(path,"r")
+    for x in f:
+        if t == 1:
+            if 'def ' in x:
+                t = 0
+                return count
+            if otherMethod in x:
+                count=count+1
+        elif str in x:
+            t = 1    
+    f.close()
+    return count
+
+
+
+
 #createGraph(loadFolder())
-create_function_graph()
+create_function_graph(loadFolder())
