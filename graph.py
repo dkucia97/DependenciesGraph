6f4379b6e7536cae3d4d249e54adf6a821040e8e
ubijaczziemniaka <56658023+ubijaczziemniaka@users.noreply.github.com>
2019-10-24 11:59:06 +0200

    Update graph.py

--- graph.py
+++ graph.py
@@ -72,26 +72,41 @@ def get_python_files(path):
 def create_function_graph(path="./"):
     g=networkx.DiGraph() # create direct graph 
     files_to_parse=get_python_files(path) #only python files
-    functions=[]
+    allFunctions=[]
+    tmpFunctions=[]
     #creating nodes
     for file in files_to_parse:
-        functions+=get_functions_names_from_file(path+"/"+file)
-        for fun in functions :
-            g.add_node(fun)
+        allFunctions+=get_functions_names_from_file(path+"/"+file)
+        tmpFunctions=get_functions_names_from_file(path+"/"+file)
+        for fun in tmpFunctions:
+            g.add_node(fun,waga = count_method_size(path+"/"+file,fun))
     #creating edges
     for file in files_to_parse:
-        for fun in functions :
-            #print("   "+fun)
-            for otherFun in functions:
+        for fun in allFunctions :
+            for otherFun in allFunctions:
                 methodCount=count_method(path+"/"+file,fun,otherFun)
                 if(methodCount>0):
-                    #print("      "+otherFun)
-                    #print("         "+str(methodCount))
-                    g.add_edge(fun,otherFun,weight = 5)
+                    g.add_edge(fun,otherFun,weight = methodCount)
 
+    ##  labels copied from other graph
+    matplotlib.pyplot.figure()
+    pos = networkx.spring_layout(g)
+    networkx.draw(g,pos, with_labels=True, font_weight='bold')
+    
+    pos_attr = {}
+    for node, coords in pos.items():
+        pos_attr[node] = (coords[0], coords[1] +00.07)
+    
+    node_attr = networkx.get_node_attributes(g, 'waga')
+    custom_node_attrs = {}
+    for node, attr in node_attr.items():
+        custom_node_attrs[node] = str(attr)
 
+    edge_labels = dict([((u,v),d['weight']) for u,v,d in g.edges(data=True)])
+    networkx.draw_networkx_edge_labels(g,pos,edge_labels = edge_labels)
 
-    networkx.draw(g, with_labels=True, font_weight='bold')
+    networkx.draw_networkx_labels(g,pos_attr, labels=custom_node_attrs)
+    ##
     matplotlib.pyplot.show()
 
 
@@ -112,6 +127,7 @@ def count_method(path, method, otherMethod):
         if t == 1:
             if 'def ' in x:
                 t = 0
+                f.close()
                 return count
             if otherMethod in x:
                 count=count+1
@@ -120,7 +136,21 @@ def count_method(path, method, otherMethod):
     f.close()
     return count
 
-
+def count_method_size(path, method): #returns function line count
+    count = 0
+    t = 0
+    str = 'def ' + method
+    f = open(path,"r")
+    for x in f:
+        if t == 1:
+           if 'def ' in x:
+                t = 0
+                return count
+           count=count+1   
+        elif str in x:
+            t = 1    
+    f.close()
+    return count
 
 
 #createGraph(loadFolder())
