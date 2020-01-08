75cce83de77beeca29bcfa018515dc932185b259
Kosko12 <piotrkrz12@gmail.com>
2020-01-09 00:30:26 +0100

    Proba nr 1 podzielenia grafu na domeny

--- graphDivision.py
+++ graphDivision.py
@@ -2,64 +2,129 @@
 import networkx
 import matplotlib.pyplot
 import graph
+matplotlib.pyplot.figure()
+
+def get_max(fun_dic):
+    val_max = 0
+    guard = 1
+    for x in range(len(fun_dic) + 1):
+        if x is not 0:
+            tmp = fun_dic.get(x)
+            print("tmp :" + str(tmp))
+            b = str(tmp).split(': ')[1].split("}")[0]
+            c = int(b)
+            print(type(c))
+            if c > val_max:
+                val_max = c
+                guard = x
+    toret = fun_dic.get(guard)
+    fun1 = str(toret).split("'")[1].split("'")[0]
+    fun2 = str(toret).split(" '")[1].split("',")[0]
+    fun_dic.pop(guard)
+    return val_max, fun1, fun2
 
 def division(path="./"):
-    g=networkx.DiGraph() # create direct graph
-    files_to_parse=graph.get_python_files(path) #only python files
-    tmp_f_t_p = files_to_parse
-    print(tmp_f_t_p)
+    g1 = networkx.DiGraph()  # create direct graph
+    g2 = networkx.DiGraph()  # create direct graph
+    g3 = networkx.DiGraph()  # create direct graph
+    files_to_parse = graph.get_python_files(path)  # only python files
+    allFunctions = []
+    fun_dic = {}
+    tmpFunctions = []
+    # creating nodes
     for file in files_to_parse:
-        count = 0
-        this_file = file
-        list_of_file_fun = graph.get_functions_names_from_file(file)
-        #
-        for func in list_of_file_fun:
-            for fl in files_to_parse:
-                if fl == this_file:
-                    g.add_node(func, weight=graph.count_method_size(path+"/"+file,func))
-                else:
-                    count = count + graph.count_method1(path+"/"+fl, func)
-        g.add_node(graph.extract_filename(file),weight = count)
-        count = 0
+        allFunctions += graph.get_functions_names_from_file(path + "/" + file)
+        tmpFunctions = graph.get_functions_names_from_file(path + "/" + file)
+    #    for fun in tmpFunctions:
+     #       g.add_node(fun, waga=graph.count_method_size(path + "/" + file, fun))
+    # creating edges
+    i = 1
+    for file in files_to_parse:
+        for fun in allFunctions:
+            if fun != "":
 
-    for file in tmp_f_t_p:
-        count = 0
-        this_file = file
-        list_of_file_fun = graph.get_functions_names_from_file(file)
-        print(list_of_file_fun)
-        for func in list_of_file_fun:
-            for fl in tmp_f_t_p:
-                if fl == this_file:
-                    tmp_count = graph.count_method1(path+"/"+fl, func)
-                    g.add_edge(func, graph.extract_filename(file),weight = tmp_count)
-                else:
-                    count = graph.count_method1(path+"/"+fl, func) + graph.count_method1(path+"/"+file, func)
+                for otherFun in allFunctions:
+                    methodCount = graph.count_method(path + "/" + file, fun, otherFun)
+                    if (methodCount > 0):
+                        #g.add_edge(fun, otherFun, weight=methodCount)
+                        if otherFun != "" and otherFun != fun:
+                            dic = {i: (fun, otherFun, {"nr": methodCount})}
+                            fun_dic.update(dic)
+                            i = i + 1
+    print(fun_dic.items())
+    i = 1
+    while fun_dic:
+        x, f1, f2 = get_max(fun_dic)
+        print(x,f1,f2)
+        if i == 1:
+            g1.add_node(f1)
+            g1.add_node(f2)
+            g1.add_edge(f1,f2,weight=x)
+        if i == 2:
+            g2.add_node(f1)
+            g2.add_node(f2)
+            g2.add_edge(f1,f2,weight=x)
+        if i == 3:
+            g3.add_node(f1)
+            g3.add_node(f2)
+            g3.add_edge(f1, f2, weight=x)
+        i = i + 1
+        if i == 4:
+            i = 1
+    ##  labels copied from other graph
+    # matplotlib.pyplot.figure()
+    pos1 = networkx.spring_layout(g1, center=[2, 1])
+    pos2 = networkx.spring_layout(g2, center=[3, 2])
+    pos3 = networkx.spring_layout(g3, center=[4, 3])
+    color_map1 = []
+    color_map2 = []
+    color_map3 = []
+    for node in g1:
+        color_map1.append("green")
+    networkx.draw(g1, pos1, node_color=color_map1, with_labels=True, font_weight='bold')
+    for node in 2:
+        color_map2.append("red")
+    networkx.draw(g2, pos2, node_color=color_map2, with_labels=True, font_weight='bold')
+    for node in g2:
+        color_map3.append("yellow")
+    networkx.draw(g3, pos3, node_color=color_map3, with_labels=True, font_weight='bold')
 
-        g.add_edge(graph.extract_filename(file),graph.extract_filename(fl),weight = count)
+    pos_attr1 = {}
+    pos_attr2 = {}
+    pos_attr3 = {}
+    for node, coords in pos1.items():
+        pos_attr1[node] = (coords[0], coords[1] + 00.07)
+    for node, coords in pos2.items():
+        pos_attr2[node] = (coords[0], coords[1] + 00.07)
+    for node, coords in pos3.items():
+        pos_attr3[node] = (coords[0], coords[1] + 00.07)
 
-    matplotlib.pyplot.figure()
-    pos = networkx.spring_layout(g, center = [0,2])
-    color_map = []
-    for node in g:
-        color_map.append("red")
-    networkx.draw(g,pos,node_color = color_map, with_labels=True, font_weight='bold')
+    node_attr1 = networkx.get_node_attributes(g1, 'waga')
+    node_attr2 = networkx.get_node_attributes(g2, 'waga')
+    node_attr3 = networkx.get_node_attributes(g3, 'waga')
+    custom_node_attrs1 = {}
+    custom_node_attrs2 = {}
+    custom_node_attrs3 = {}
+    for node, attr in node_attr1.items():
+        custom_node_attrs1[node] = str(attr)
+    for node, attr in node_attr2.items():
+        custom_node_attrs2[node] = str(attr)
+    for node, attr in node_attr3.items():
+        custom_node_attrs3[node] = str(attr)
 
-    pos_attr = {}
-    for node, coords in pos.items():
-        pos_attr[node] = (coords[0], coords[1] + 00.05)
+    edge_labels1 = dict([((u, v), d['weight']) for u, v, d in g1.edges(data=True)])
+    edge_labels2 = dict([((u, v), d['weight']) for u, v, d in g2.edges(data=True)])
+    edge_labels3 = dict([((u, v), d['weight']) for u, v, d in g3.edges(data=True)])
 
-    node_attr = networkx.get_node_attributes(g, 'weight')
-    custom_node_attrs = {}
-    for node, attr in node_attr.items():
-        custom_node_attrs[node] = str(attr)
+    networkx.draw_networkx_edge_labels(g1, pos1, edge_labels=edge_labels1)
+    networkx.draw_networkx_edge_labels(g2, pos2, edge_labels=edge_labels2)
+    networkx.draw_networkx_edge_labels(g3, pos3, edge_labels=edge_labels3)
 
-    edge_labels = dict([((u,v),d['weight']) for u,v,d in g.edges(data=True)])
-    networkx
+    networkx.draw_networkx_labels(g1, pos_attr1, labels=custom_node_attrs1)
+    networkx.draw_networkx_labels(g2, pos_attr2, labels=custom_node_attrs2)
+    networkx.draw_networkx_labels(g3, pos_attr3, labels=custom_node_attrs3)
 
-    networkx.draw_networkx_edge_labels(g,pos,edge_labels = edge_labels)
-
-    networkx.draw_networkx_labels(g,pos_attr, labels=custom_node_attrs)
-    matplotlib.pyplot.show()
 
 lf = graph.loadFolder()
 division(lf)
+matplotlib.pyplot.show()
